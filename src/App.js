2685974885405f1c85a40ef5a7abe6152e07c44a
mahen2-cmd <mahenmughal2@gmail.com>
2023-08-11 18:52:11 +0500

    The third section is still in the making but it's becoming quite challenging.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import SecondSection from './SecondSection';
 import TopBar from './TopBar';
+import computer from "./png_images/icon-access-anywhere.png";
+
+import security from "./png_images/icon-security.png";
+// import files from ""
+
+// import files from "./png_images/icon-any-file.png";
 // import logo from "./png_images/logo.png";
 
 
@@ -12,8 +18,57 @@ function App() {
           <TopBar />
           <SecondSection />
 
+          <br />
+          <br />
+          <br />
 
+          <div class="w-full h-auto flex flex-col items-center text-center">
+              <div class=" w-1/2 flex flex-row items-center justify-between text-center bg-green-900">
+                  <div class="w-auto flex flex-col items-center ">
+                      <img
+                          src={computer}
+                          alt="Logo"
+                          width="70px"
+                          height="70px"
+                      />
 
+                      <br />
+
+                      <div class="font-bold text-white text-lg text-center">
+                          Access your files, anywhere
+                      </div>
+
+                      <div class="font-regular text-white text-xs text-center">
+                          The ability to use a smartphone, tablet, or computer{" "}
+                          to access your account means your files follow you
+                          everywhere.
+                      </div>
+                  </div>
+
+                  <div class="w-auto flex flex-col items-center">
+                      {/* <br /> */}
+
+                      <img
+                          src={security}
+                          alt="Logo"
+                          width="70px"
+                          height="70px"
+                      />
+
+                      {/* <br /> */}
+
+                      <div class="font-bold text-white text-lg text-center">
+                          Security you can trust
+                      </div>
+
+                      <div class="font-regular text-white text-xs text-center">
+                          The ability to use a smartphone, tablet, or computer{" "}
+                          to access your account means your files follow you
+                          everywhere.
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
   );
 }
